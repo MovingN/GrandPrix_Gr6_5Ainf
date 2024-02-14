e500de5941b487be8b4abd57b0f34334a4087088
Giorgio Nucciarelli <giorgionucciarelli1@gmail.com>
2024-02-14 21:08:17 +0100

    Revert "Popolamento degli attributi e dei metodi delle classi"
    This reverts commit a9d0e51f0394ff1cc2ea3fa1af7353b64a49e1d1.

--- src/grandprix6/Circuito.java
+++ src/grandprix6/Circuito.java
@@ -9,28 +9,5 @@ package grandprix6;
  * @author federico,giorgio,razvan
  */
 public class Circuito {
-
-    private int lunghezza;
-    private int giri;
-
-    public Circuito(int lunghezza, int giri) {
-        this.lunghezza = lunghezza;
-        this.giri = giri;
-    }
-
-    public int getLunghezza() {
-        return lunghezza;
-    }
-
-    public int getGiri() {
-        return giri;
-    }
-
-    public void setLunghezza(int lunghezza) {
-        this.lunghezza = lunghezza;
-    }
-
-    public void setGiri(int giri) {
-        this.giri = giri;
-    }
+    
 }
--- src/grandprix6/GrandPrix6.java
+++ src/grandprix6/GrandPrix6.java
@@ -16,8 +16,5 @@ public class GrandPrix6 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-
-    Giocatore g1 = new Giocatore("Moving", "bruCoLA");
-
-    g1.start ();
+    
 }
